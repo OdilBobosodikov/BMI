d1703c77ac0c874f66c3593c8dffbf9bd2fc5d19
Odiljon Bobosodikov <bobosodikov0d@gmail.com>
2024-01-18 16:06:33 +0500

    Add project files.

---
+++ MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BMI.MVVM.Models;
+
+namespace BMI.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+        public BMIModel BMI { get; set; }
+        public BMIViewModel()
+        {
+            BMI = new BMIModel()
+            {
+                Height = 186,
+                Weight = 86,
+            };
+        }
+    }
+}
